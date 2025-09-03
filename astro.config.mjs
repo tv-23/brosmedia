ba5dc60290f8126b83bab8442df00bb18aba19f1
tv-23 <tvelqjiri@gmail.com>
2025-09-03 23:37:30 +0100

    jk

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-  compressHTML: true, // Enable HTML compression
-  build: {
-    format: 'directory', // Better caching
-  },
-  vite: {
+    compressHTML: true, // Enable HTML compression
     build: {
-      minify: 'esbuild', // Ensure minification
+        format: 'directory', // Better caching
+    },
+    vite: {
+        build: {
+            minify: 'esbuild', // Ensure minification
+        }
     }
-  }
 });
